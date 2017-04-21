8410e809876439911a90610b64626248c79f665b
bdy <bodeyu461054993@qq.com>
2017-04-21 14:23:14 +0800

    webarticle类可以直接返回一个含有title和text的字典

--- webarticle.py
+++ webarticle.py
@@ -1,8 +1,6 @@
 import requests
 from urllib.request import urlopen
 import re
-import chardet
-import numpy as np
 import matplotlib.pylab as plt
 import urllib
 import time
@@ -10,14 +8,12 @@ from bs4 import BeautifulSoup
 
 
 class webarticle(object):
-
     def __init__(self, url='', keyword='', num=1):
 
         self.url = url
         self.keyword = keyword
-        self.keyword_url_num = num
-
-        self.begin = self.end = 0  # 正文开头行和结尾行
+        self.title = ''
+        self.text = ''
 
         if url != '':
             self.get_web_article()
@@ -49,79 +45,92 @@ class webarticle(object):
         if out_url != '':
             self.url = out_url
 
-        self.encoder = self.get_url_chardet(self.url)
         try:
-            self.req = requests.get(self.url)
-            self.req.encoding = self.encoder
+            self.req = requests.get(self.url, timeout=5)
+            self.req.encoding = self.req.headers['content-type'].split('=')[1]
             self.text = self.req.text
         except:
+            self.text = ''
             print('parser error, cannot get text, url:', self.url)
             return
 
-        if '百度贴吧' in self.text or '发表于' in self.text or '百度经验' in self.text or '百度文库' in self.text or len(
-                self.text) < 1000:
+        if '百度' in self.title or \
+                        '爱奇艺' in self.title or \
+                        '优酷' in self.title or \
+                        '腾讯视频' in self.title or \
+                        len(self.text) < 1000 or \
+                        self.req.status_code != 200:
+            self.text = ''
             print('web content error, cannot get article, url:', self.url)
             return
+
+        self.title = re.findall(r'<title>(.*?)</title>', self.text)[0]
 
         self.clean_text()
 
         lines = self.text.split('\n')
         article = []
         for i, line in enumerate(lines):
-            if len(line) > 120 and line.count('*') < 5:  # 如果一段超过120个字 直接认为肯定是正文，这样保证article里面肯定不会空
+
+            if len(line) > 120 and \
+                    len(re.findall(r'\W', line)) / len(line) < 0.2 and \
+                    len(re.findall(r'[a-zA-Z]', line)) / len(line) < 0.2:
+
+                # 如果一段超过120个字 直接认为肯定是正文，这样保证article里面肯定不会空
                 article.append(i)
+
+        begin = end = 0  # 正文开头行和结尾行
 
         if len(article) == 0:
             print('cannot get web article, url:', self.url)
             self.text = ''
             return
-
         elif len(article) == 1:
-            self.begin = self.end = article[0]
+            begin = end = article[0]
         else:
             article.sort()
-            self.begin = article[0]
-            self.end = article[-1]
+            begin = article[0]
+            end = article[-1]
 
         while True:
-            if self.begin <= 2:
+            if begin <= 2:
                 break
             else:
-                if lines[self.begin - 1] == '':
-                    if lines[self.begin - 2] == '':
+                if lines[begin - 1] == '':
+                    if lines[begin - 2] == '':
                         break
                     else:
-                        if not self.if_adv(lines[self.begin - 2]):
-                            self.begin -= 2
+                        if not self.if_adv(lines[begin - 2]):
+                            begin -= 2
                         else:
                             break
                 else:
-                    if self.if_adv(lines[self.begin - 1]):
+                    if self.if_adv(lines[begin - 1]):
                         break
                     else:
-                        self.begin -= 1
+                        begin -= 1
 
         while True:
-            if self.end >= len(lines) - 2:
+            if end >= len(lines) - 2:
                 break
             else:
-                if lines[self.end + 1] == '':
-                    if lines[self.end + 2] == '':
+                if lines[end + 1] == '':
+                    if lines[end + 2] == '':
                         break
                     else:
-                        if not self.if_adv(lines[self.end + 2]):
-                            self.end += 2
+                        if not self.if_adv(lines[end + 2]):
+                            end += 2
                         else:
                             break
                 else:
-                    if self.if_adv(lines[self.end + 1]):
+                    if self.if_adv(lines[end + 1]):
                         break
                     else:
-                        self.end += 1
+                        end += 1
 
         # 对已经获得的正文进行重新审核 部分广告和正文之间可能混在一起
         self.text = ''
-        for k in range(self.begin, self.end + 1):
+        for k in range(begin, end + 1):
             txt = lines[k].replace('&ldquo;', '“').replace('&rdquo;', '”')
             if txt.count('*') > 5 or len(txt) < 10:
                 if len(txt) < 10:
@@ -134,49 +143,88 @@ class webarticle(object):
             else:
                 self.text += txt.replace('*', '') + '\n'
 
+        if self.text != '':
+            dic = {}
+            dic['title'] = self.title
+            dic['text'] = self.text
+            return dic
+
     def get_url_from_net(self, keyword='', num=1):
+
+        page = num // 10  # url总共页数
 
         if self.keyword == '':
             self.keyword = keyword
 
-        self.keyword = re.sub(r'\s+', ' ', self.keyword)
+        re_html = re.compile(r'href="(.*?)"')  # 提取百度返回的网页
 
-        # 根据关键字从百度获取第一页的url
-        baidu_url = 'http://www.baidu.com/s?wd=' + urllib.parse.quote(self.keyword)
-        htmlpage = urllib.request.urlopen(baidu_url, timeout=5).read()
-        soup = BeautifulSoup(htmlpage, 'html.parser', from_encoding='utf-8')
-
-        content = soup.find('div', id='wrapper').find('div', id='wrapper_wrapper').find('div', id='container'). \
-            find('div', id='content_left')
-        html_tags = content.find_all('div', class_='result c-container ')
-        htmls = []
-        for html_tag in html_tags:
-            new = html_tag.find('h3', class_='t')
-            htmls.append(str(new).split('}" href="')[1].split('" target="')[0])
-        urls = self.GetRealUrl(htmls)
+        # 根据关键字从百度获取url
         realurls = []
-        for url in urls:
-            try:
-                response = urllib.request.urlopen(url)
-                realurl = response.geturl()
-                requests.get(realurl, timeout=2)
-                realurls.append(realurl)
-            except:
-                pass
 
-        return realurls[0:num]
+        if page > 0:
+            # url翻页的形式 http://www.baidu.com/s?wd=keyword&pn=(page-1)*10
+
+            for i in range(page + 1):
+
+                print(i)
+
+                baidu_url = 'http://www.baidu.com/s?wd=' + urllib.parse.quote(self.keyword) + '&pn=' + str(i) + '0'
+                htmlpage = urllib.request.urlopen(baidu_url, timeout=5).read()
+                soup = BeautifulSoup(htmlpage, 'html.parser', from_encoding='utf-8')
+
+                content = soup.find('div', id='wrapper').find('div', id='wrapper_wrapper').find('div',
+                                                                                                id='container').find(
+                    'div', id='content_left')
+                html_tags = content.find_all('div', class_='result c-container ')
+                htmls = []
+                for html_tag in html_tags:
+                    new = html_tag.find('h3', class_='t')
+                    new_url = re_html.findall(str(new))[0]
+                    print(new.get_text())
+                    if '百度' in new.get_text():
+                        print('百度文库一类的,不能用 url:', new_url)
+                        continue
+                    htmls.append(new_url)
+
+                if i == page:
+                    num -= page * 10
+                    realurls.extend(self.GetRealUrl(htmls)[0:num])
+                else:
+                    realurls.extend(self.GetRealUrl(htmls))
+
+        else:
+            baidu_url = 'http://www.baidu.com/s?wd=' + urllib.parse.quote(self.keyword)
+            htmlpage = urllib.request.urlopen(baidu_url, timeout=5).read()
+            soup = BeautifulSoup(htmlpage, 'html.parser', from_encoding='utf-8')
+
+            content = soup.find('div', id='wrapper').find('div', id='wrapper_wrapper').find('div', id='container'). \
+                find('div', id='content_left')
+            html_tags = content.find_all('div', class_='result c-container ')
+            htmls = []
+            for html_tag in html_tags:
+                new = html_tag.find('h3', class_='t')
+                new_url = re_html.findall(str(new))[0]
+                if '百度' in new.get_text():
+                    print('百度文库一类的,不能用 url:', new_url)
+                    continue
+                htmls.append(new_url)
+
+            realurls.extend(self.GetRealUrl(htmls))
+
+        return realurls
 
     def store_article(self, path='', name=''):
-        if len(self.text) < 200:
-            print('content is little, url:', self.url)
+        if self.text == '':
             return
+        else:
+            print('find article, url:', self.url)
 
         if path == '':
             if name == '':
                 name = str(time.time()) + '.txt'
             else:
                 name += '.txt'
-            with open(name, 'w+') as f:
+            with open(name, 'w+', encoding='utf-8') as f:
                 f.write(self.text)
             f.close()
         else:
@@ -202,15 +250,6 @@ class webarticle(object):
         return realurls
 
     @staticmethod
-    def get_url_chardet(url):
-
-        data = urlopen(url).read()
-        # 获取源代码之后找到网页编码方式 再获取
-        encoder = chardet.detect(data)
-
-        return encoder['encoding']
-
-    @staticmethod
     def if_adv(line):
         if len(line) > 40:
             return False
@@ -222,8 +261,3 @@ class webarticle(object):
                     return True
                 else:
                     return False
-
-if __name__ == "__main__":
-    w = webarticle()
-    w.get_web_article('http://www.yjbys.com/gongwuyuan/show-506089.html')
-    print(w.text)
